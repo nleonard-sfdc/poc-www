f2980b9f47d81427997ade5a48ea8ad83ab71728
ned leonard <nleonard@salesforce.com>
2018-08-17 21:40:19 -0700

    add validation

--- root/src/main/java/servlet/LeadFilter.java
+++ root/src/main/java/servlet/LeadFilter.java
@@ -4,6 +4,7 @@ package servlet;/*
 
 
 import org.apache.commons.io.IOUtils;
+import org.apache.http.Header;
 import org.apache.http.client.fluent.Form;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.client.fluent.Response;
@@ -23,6 +24,9 @@ public class LeadFilter extends HttpServlet {
 
     public static final String FORWARDING_LEADCAPTURE = "https://sheltered-plateau-61056.herokuapp.com/leadcapture/ForwardingLeadCaptureServlet";
 
+    public static final String START_PAGE = "/form/demo.html";
+    public static final String RESULT_PAGE = "/success.html";
+
     @Override protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         doPost(req, resp);
     }
@@ -30,6 +34,8 @@ public class LeadFilter extends HttpServlet {
     @Override protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         Form form = Form.form();
+        form.add("resultPage", RESULT_PAGE);
+        form.add("startPage", START_PAGE);
 
         Enumeration<String> params = req.getParameterNames();
         while(params.hasMoreElements()){
@@ -40,6 +46,14 @@ public class LeadFilter extends HttpServlet {
         Response leadResponse = Request.Post(FORWARDING_LEADCAPTURE)
             .addHeader("True-Client-IP", req.getRemoteAddr())
             .bodyForm(form.build()).execute();
+
+        Header header = leadResponse.returnResponse().getHeaders("Location")[0];
+        String location = header.getValue();
+        if(!location.contains(RESULT_PAGE)){
+            resp.sendRedirect(location);
+            return;
+        }
+
 
         String confPath = req.getRequestURI();
         IOUtils.copy(req.getServletContext().getResourceAsStream(confPath), resp.getOutputStream());
